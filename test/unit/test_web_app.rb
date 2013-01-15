2a5b12890898fc112b9b66b2fc32998f6ca81105
Steven R. Baker <steven@stevenrbaker.com>
2013-01-14 23:41:07 -0400

    fix some backwards assertions

--- test/unit/test_web_app.rb
+++ test/unit/test_web_app.rb
@@ -10,27 +10,27 @@ class TestWebApp < MiniTest::Unit::TestCase
 
   def test_returns_hello_world_on_index
     response = @app.test_request(:get, '/')
-    assert_equal response, "hello world"
+    assert_equal "hello world", response
   end
 
   def test_returns_goodbye_on_bye
     response = @app.test_request(:get, '/bye')
-    assert_equal response, "goodbye"
+    assert_equal "goodbye", response
   end
 
   def test_has_hello_name
     response = @app.test_request(:get, '/hello/jon')
-    assert_equal response, "hello jon"
+    assert_equal "hello jon", response
   end
 
   def test_returns_for_hi_with_param
     response = @app.test_request(:get, '/hi', :qsname => 'jon')
-    assert_equal response, "hi jon"
+    assert_equal "hi jon", response
   end
 
   def test_work_with_get_wrapped_method
     response = @app.test_get '/'
-    assert_equal response, "hello world"
+    assert_equal "hello world", response
   end
 
   def test_return_nil_for_wrong_route
@@ -40,24 +40,24 @@ class TestWebApp < MiniTest::Unit::TestCase
 
   def test_works_with_post_data
     response = @app.test_post '/goodnight', :name => 'jon'
-    assert_equal response, 'post goodnight jon'
+    assert_equal 'post goodnight jon', response
   end
 
   def test_works_with_pu
     response = @app.test_put '/goodnight', :name => 'jon'
-    assert_equal response, 'put goodnight jon'
+    assert_equal 'put goodnight jon', response
   end
 
   def test_works_with_env
     @app.env = { :test => 'hello' }
     response = @app.test_post '/showenv'
-    assert_equal response, 'env[test] hello'
+    assert_equal 'env[test] hello', response
   end
 
   def test_works_with_session
     @app.session = { :test => 'hello' }
     response = @app.test_post '/showsession'
-    assert_equal response, 'session[test] hello'
+    assert_equal 'session[test] hello', response
   end
 
   def test_instance_variable_retrieval
